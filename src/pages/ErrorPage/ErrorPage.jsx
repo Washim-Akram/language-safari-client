7503fa96a83096da0ecadd7d682fea7368bf897d
Washim-Akram <muhammadwashimakram@gmail.com>
2023-06-08 00:28:55 +0600

    Implemented ErrorPage and use lottie files for animation

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,26 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+import { Link, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const { error } = useRouteError();
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <Player
+        autoplay
+        loop
+        src="https://assets8.lottiefiles.com/packages/lf20_bdyxev2w.json"
+        style={{ height: "350px", width: "350px" }}
+      ></Player>
+
+      <p className="font-medium my-4 text-red-600 md:text-[18px]">
+        {error.message}
+      </p>
+
+      <Link to="/">
+        <button className="btn btn-neutral">Back to Home</button>
+      </Link>
+    </div>
+  );
+};
+
+export default ErrorPage;
